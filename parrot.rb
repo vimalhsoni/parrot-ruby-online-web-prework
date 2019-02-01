1c143d0a6c568681b340da9e5aec225b037b9187
Vimal Soni <vimal.h.soni@gmail.com>
2019-02-01 21:29:20 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,6 +1,6 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot(phrase = '"Sqwauak!')
+def parrot(phrase = '"Sqwuak!')
   puts phrase
   return phrase
 end
